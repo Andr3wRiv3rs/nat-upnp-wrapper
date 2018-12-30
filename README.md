32675d34ede677263d5a76f4be29eef7fc632158
Andr3wRiv3rs <andrewarivers@gmail.com>
2018-12-30 01:34:03 -0800

    Accepts Arrays

--- README.md
+++ README.md
@@ -3,23 +3,20 @@ This is a wrapper for the nat-upnp module.
 
 ##Script Usage
 
-Map port:
+**Map port:**
 ```js
 const mapper = require('nat-upnp-wrapper');
 mapper.map(80).then(response => console.log(response.success));
 ```
+Format: `public OR public, private OR [public, [public,private], ..]`
 
-With private port:
-```js
-mapper.map(80, 80).then(response => ...);
-```
-
-Unmap port:
+**Unmap port:**
 ```js
 mapper.unmap(80).then(response => ...);
 ```
+Format: `public OR [public, ..]`
 
-Get Public IP Address:
+**Get Public IP Address:**
 ```js
 mapper.ip().then(ip => console.log(ip));
 ```
--- mapper.js
+++ mapper.js
@@ -2,15 +2,37 @@ const natUpnp = require('nat-upnp')
 const client = natUpnp.createClient()
 
 const map = (public,private) => new Promise(res => {
-    private = private || public;
-    client.portMapping({
-        public, 
-        private, 
-        ttl: 1
-    }, err => err ? res({success:false,err}) : res({success:true}))
-})
+    if (Array.isArray(public)) {
+        for (let i in public) if (!Array.isArray(public[i])) public[i] = [public[i],public[i]];
 
-const unmap = public => new Promise(res => client.portUnmapping({public}, err => err ? res({success:false,err}) : res({success:true})));
+        let i = 0;
+        const loop = (p,pr) => {
+            client.portMapping({ p, pr, ttl: 1 }, err => {
+                console.log(p,pr)
+                if (err) res({success:false,err})
+
+                else if (i<ports.length-1) {
+                    i++;
+                    loop((ports[i][0],ports[i][1]));
+                } 
+
+                else res({success:true})
+            });
+        };
+
+        loop((ports[i][0],ports[i][1]));
+    } else {
+        private = private || public;
+        client.portMapping({ public, private, ttl: 1 }, err => err ? res({success:false,err}) : res({success:true}));
+    }
+});
+
+const unmap = public => new Promise(res => {
+    const um = p => client.portUnmapping({p}, err => err ? res({success:false,err}) : res({success:true}))
+    if (Array.isArray(public)) for (let port of public) um(port)
+    else um(public)
+});
+
 const ip = () => new Promise(res => client.externalIp((err, ip) => err ? res({success:false,err}) : res({success:true,ip})))
 
 exports.map = map;
